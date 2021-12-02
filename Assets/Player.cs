1b73517260162e851d264279a32c1e93f27d2629
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2021-12-02 21:52:59 +0900

    2
    Project folder.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello, Unity!");
+
+        int hp = 100;
+        float DPS = 4.6f;
+        string name = "¿Ã∏ß";
+        bool isMove = false;
+
+        Debug.Log("HP : " + hp);
+        Debug.Log("DPS : " + DPS);
+        Debug.Log("NAME : " + name);
+        Debug.Log("ISMOVE : " + isMove);
+
+        for (int i = 0; i < 10; i++)
+        {
+            hp--;
+            Debug.Log("HP : " + hp);
+        }
+
+        if(DPS >= 3f)
+        {
+            name = "ABC";
+            Debug.Log("NAME : " + name);
+        }
+        else if(!isMove)
+        {
+            isMove = true;
+            Debug.Log("ISMOVE : " + isMove);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveVec;
+
+        moveVec = new Vector3(h, 0f, v).normalized * moveSpeed * Time.deltaTime;
+
+        transform.position += moveVec;
     }
 }
