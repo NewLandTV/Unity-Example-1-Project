34bd2a31bc7ed1883c921878b8acdcdd8fbd3e8c
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2022-01-09 17:27:52 +0900

    Example_4

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void ClickButton1()
+    {
+        Debug.Log("버튼이 눌림");
+    }
+}
